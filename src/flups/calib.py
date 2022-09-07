c82e41b07e36be7f39a3e0cc2064f150c2dcde7a
SPalato <7659022+spalato@users.noreply.github.com>
2022-09-07 13:20:53 +0200

    Added latest calibration.
    Changed calibration to use string name instead of date structure.

--- src/flups/calib.py
+++ src/flups/calib.py
@@ -6,6 +6,9 @@ from datetime import date as datetype
 from dataclasses import dataclass
 import numpy as np
 import yaml
+import logging
+
+logger = logging.getLogger(__name__)
 
 # I need a small object for the calibrations..
 @dataclass(frozen=True)
@@ -17,27 +20,27 @@ class calibration:
 
     Parameters
     ----------
-    date : datetime.date
-        Calibration date
+    date : str
+        Calibration name (typically a date)
     b0 : float
         Intercept
     b1 : float
         Slope
     """
-    date: datetype
+    name: datetype
     b0: float
     b1: float
     
     @classmethod
-    def from_record(cls, date:datetype, b0: float, b1: float,
+    def from_record(cls, name:str, b0: float, b1: float,
         zero_indexed: bool=True):
         """
         Load a calibration from a parameter record.
 
         Parameters
         ----------
-        date : datetime.date, str
-            Calibration date. If of type `str`, expects ISO format (YYYY-MM-DD)
+        name : str
+            Calibration name, please start with a date in ISO format (YYYY-MM-DD).
         b0 : float
             Intercept
         b1 : float
@@ -46,11 +49,13 @@ class calibration:
             Indicate if the supplied parameters use 0-based indexing. If 
             `zero_indexed` is False, the 
         """
-        if isinstance(date, str):
-            date = datetype.fromisoformat(date)
+        try:
+            date = datetype.fromisoformat(name[:10])
+        except ValueError:
+            logger.warn(f"Calibration name does not start by valid date: {name}")
         if not zero_indexed: # convert from 1-indexing to 0-indexing
             b0 = b0 + b1
-        return cls(date, b0, b1)
+        return cls(name, b0, b1)
 
     def calibrate(self, i):
         """Converts pixel index `i` to wavelength, in nm.
@@ -69,12 +74,12 @@ DEFAULT_CALIB_FILE = pth.join(
 
 def load_latest():
     calibs = list_calibrations()
-    return max(calibs, key=lambda v: v.date)
+    return max(calibs, key=lambda v: v.name)
 
 
 def list_calibrations(file=DEFAULT_CALIB_FILE): # this can handle specific directories
     with open(file, "r") as f:
         records = yaml.safe_load(f)
-    calibrations = [calibration.from_record(date, **values) 
-        for date, values in records.items()]
+    calibrations = [calibration.from_record(name, **values) 
+        for name, values in records.items()]
     return calibrations
